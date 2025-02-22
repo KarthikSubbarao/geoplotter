ab196e6ca57d478fd7f1f53a0b97c3bf94f4c2c7
KarthikSubbarao <karthikrs2021@gmail.com>
2025-02-22 15:20:02 -0800

    support polygon search, list locations and geoadd, loading from valkey, flushing data in valkey
    Signed-off-by: KarthikSubbarao <karthikrs2021@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # geoplotter
 Geo Coord Visualizer of Locations and Polygons Boundaries that interacts with Valkey
+
+pyenv local 3.8.10
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+if which pyenv > /dev/null; then
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
+fi
+python --version   # Should say 3.8
+
+pip install -r requirements.txt
+
+python ./web.py 
--- web.py
+++ web.py
@@ -9,25 +9,53 @@ def home():
     return render_template_string(open('template.html').read())
 
 # Route to handle the polygon submission
-@app.route('/submit_polygon', methods=['POST'])
-def submit_polygon():
+@app.route('/save_locations', methods=['POST'])
+def save_locations():
+    import valkey
+    count = 0
     try:
         data = request.get_json()  # Get the polygon data from the frontend
-        coordinates = data.get('coordinates')
-
-        # Process coordinates (e.g., save to database, calculate area, etc.)
-        print("Received coordinates:", coordinates)
-
-        # For demonstration, just return the received coordinates
-        return jsonify({"status": "success", "coordinates": coordinates})
-    
+        location_markers = data.get('locationMarkers', [])
+        print("Received location_markers: " + str(location_markers))    
+        r = valkey.StrictValkey(host='localhost', port=7000, db=0)
+        key = "MAP"
+        for marker in location_markers:
+            lon = marker.get('lon')
+            lat = marker.get('lat')
+            name = marker.get('name')
+            resp = r.execute_command('GEOADD', key, lon, lat, name)
+            print(resp)        
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
+        print("Error in save_locations: " + str(e))
+        return jsonify({"success": False, "error": "Error while adding locations"}), 400
+    return jsonify({"success": True})        
+
+# Route to handle the polygon submission
+@app.route('/submit_polygon', methods=['POST'])
+def submit_polygon():
+    import valkey
+    # Set up valkey connection
+    geo_data = []
+    try:
+        data = request.get_json()  # Get the polygon data from the frontend
+        cmd = str(data.get('cmd'))
+        print("Received cmd:", cmd)
+        r = valkey.StrictValkey(host='localhost', port=7000, db=0)
+        # Fetch all members from the search
+        geolist = r.execute_command(cmd)
+        for item in geolist:
+            geo_data.append({
+                "member": item.decode("utf-8"),  # Decode byte string to string
+            })
+    except Exception as e:
+        print("error in submit_polygon: " + str(e))
+    print(geo_data)
+    return jsonify({"geo_data": geo_data})
 
 @app.route('/load_valkey_data', methods=['GET'])
 def load_valkey_data():
     import valkey
-    geo_data_key = "map"
+    geo_data_key = "MAP"
     geo_data = []
     # Set up valkey connection
     try:
@@ -46,8 +74,21 @@ def load_valkey_data():
                     "lng": pos[0][0]
                 })
     except Exception as e:
-        print("error in fetch_geo_data: " + str(e))
+        print("error in load_valkey_data: " + str(e))
     return jsonify({"geo_data": geo_data})
+
+
+@app.route('/flush_valkey', methods=['GET'])
+def flush_valkey():
+    import valkey
+    # Set up valkey connection
+    try:
+        r = valkey.StrictValkey(host='localhost', port=7000, db=0)
+        r.execute_command("FLUSHALL")
+    except Exception as e:
+        print("error in flush_valkey: " + str(e))
+        return jsonify({"success": False})
+    return jsonify({"success": True})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
